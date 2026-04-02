3e147c0b0d69248b286c84f1871bd1808d46e406
Amurag <anuragsinghisgood@gmail.com>
2026-04-02 14:22:47 +0530

    fixed the issue in the dashboard login-2

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
-// const BASE_URL = "https://hiring-platform-render-hosted.onrender.com";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://hiring-platform-render-hosted.onrender.com";
 
 export default BASE_URL;
