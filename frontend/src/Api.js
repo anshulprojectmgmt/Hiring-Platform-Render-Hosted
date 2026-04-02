808d7faee16c0cbc604d3618f2cf5952f189fad8
Amurag <anuragsinghisgood@gmail.com>
2026-04-02 12:20:32 +0530

    changed the backend url

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:5000";
-// const BASE_URL = "https://api.realtyai.in";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://hiring-platform-render-hosted.onrender.com";
+
 export default BASE_URL;
